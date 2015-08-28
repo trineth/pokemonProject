8b302fbad4ac3aeca79ba1f7baecde41dd394c40
stevenwu <ventesw@gmail.com>
2015-08-28 10:03:29 -0700

    Setting up framework

--- fieldObject.cpp
+++ fieldObject.cpp
@@ -1,49 +1,80 @@
-
-
 class FieldObject
 {
 public:
-	int position[2];
+    int position[2];
+    int spriteLoc;
+    int visible;
 
-	FieldObject(int x, int y) {
-		position = [x, y]
-	}
+    FieldObject()
+    {
+    }
 
-	virtual void update();
+    virtual void update();
 };
 
 
-class NPC : public FieldObject
+class Player : public FieldOjbect
 {
 public:
 
-	NPC(int x, int y): FieldObject(x, y)
-	{
-	}
-
-	void update()
 };
 
 
-class Trainer : public NPC
+class NPObject : public FieldObject
 {
+public:
+    int moveType;
+    int triggerType;
+    int dialogue;
 
+    FieldObject()
+    {
+    }
 };
 
 
-class ImmoveableObject : public FieldObject
+class NPC : public NPObject
 {
+protected:
+public:
+    NPC()
+    {
+    }
 
+    void update()
 };
 
 
-class MoveableObject : public FieldObject
+class Trainer : public NPObject
 {
+public:
+    int pokemonCount;
+    int pokemon;
+    bool defeated = false;
 
+    Trainer()
+    {
+    }
 };
 
 
-class Player : public FieldObject
+class Structure : public NPObject
 {
+public:
+    
+};
 
+
+class Exit : public NPObject
+{
+};
+
+
+class FieldItem : public NPObject
+{
+};
+
+
+class GroundType : public NPObject
+{
 };
